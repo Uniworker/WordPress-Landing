26b0efa66e188dfcb42a75866d3e2a951e412e1f
Uniworker <markyatsyn23@gmail.con>
2022-09-24 04:43:02 +0300

    styling accomplished

--- js/plugins.js
+++ js/plugins.js
@@ -20,5 +20,22 @@
     }
   }
 }());
-
-// Place any jQuery/helper plugins in here.
+$(document).ready(function () {
+  $('.gallery__inner').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function (item) {
+        return item.el.attr('title');
+      }
+    }
+  });
+});
